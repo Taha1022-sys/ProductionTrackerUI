94582567e487a96948d97e82c4eee0347a3324cb
Taha <tahafurkan.39@gmail.com>
2025-10-03 01:43:07 +0300

    Update API base URL to include '/api' path

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL ='https://production-tracker-taha-aee2bubdg9hyg9g6.francecentral-01.azurewebsites.net';
+const API_BASE_URL ='https://production-tracker-taha-aee2bubdg9hyg9g6.francecentral-01.azurewebsites.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
